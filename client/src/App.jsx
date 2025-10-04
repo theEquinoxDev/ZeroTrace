fd875683aa8cdc44e1f1358cc3270e4cd187c71e
Aditya <adityasuri11ss@gmail.com>
2025-10-04 16:12:08 +0530

    Added Toastify

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,16 +4,21 @@ import SessionForm from "./components/SessionForm";
 import Rooms from "./components/Rooms";
 import Room from "./pages/Room";
 import NotFound from "./pages/NotFound";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/session" element={<SessionForm />} />
-      <Route path="/rooms" element={<Rooms />} />
-      <Route path="/room/:roomId" element={<Room />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/session" element={<SessionForm />} />
+        <Route path="/rooms" element={<Rooms />} />
+        <Route path="/room/:roomId" element={<Room />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <ToastContainer position="top-right" autoClose={3000} />
+    </>
   );
 }
 
