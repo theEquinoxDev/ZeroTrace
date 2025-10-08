93e74b714bc4b788e6449cfb7897081867e1a665
Aditya <adityasuri11ss@gmail.com>
2025-10-08 14:01:01 +0530

    UI enhancement

--- client/src/components/RoomForm.jsx
+++ client/src/components/RoomForm.jsx
@@ -30,34 +30,37 @@ export default function RoomForm({ onCreated }) {
   };
 
   return (
-    <div className="flex flex-col gap-3 mt-4 p-4 bg-white rounded-xl shadow-md max-w-md">
+    <div className="flex flex-col gap-3 mt-4 p-4 bg-gradient-to-br from-black via-neutral-900 to-gray-950 rounded-2xl shadow-lg max-w-md border border-gray-800 ">
       <input
         value={name}
         onChange={(e) => setName(e.target.value)}
         placeholder="Room name"
-        className="p-3 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-indigo-400"
+        className="p-3 bg-neutral-900 text-gray-200 placeholder-gray-500 border border-gray-700 rounded-xl focus:outline-none focus:ring-2 focus:ring-gray-400"
       />
+
       <select
         value={visibility}
         onChange={(e) => setVisibility(e.target.value)}
-        className="p-3 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-indigo-400"
+        className="p-3  bg-neutral-900 text-gray-200 border border-gray-700 rounded-xl focus:outline-none focus:ring-2 focus:ring-gray-400"
       >
         <option value="public">Public</option>
         <option value="private">Private</option>
       </select>
+
       {visibility === "private" && (
         <input
           type="text"
           value={inviteCode}
           onChange={(e) => setInviteCode(e.target.value)}
           placeholder="Invite code for private room"
-          className="p-3 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-indigo-400"
+          className="p-3 bg-neutral-900 text-gray-200 placeholder-gray-500 border border-gray-700 rounded-xl focus:outline-none focus:ring-2 focus:ring-gray-400"
         />
       )}
+
       <button
         onClick={handleCreate}
         disabled={loading}
-        className="bg-indigo-600 text-white px-6 py-3 rounded-xl hover:bg-indigo-700 transition disabled:opacity-50"
+        className="bg-gradient-to-r from-gray-200 to-gray-400 text-black px-6 py-3 rounded-xl font-semibold shadow-md hover:scale-105 transition-all duration-300 disabled:opacity-50"
       >
         {loading ? "Creating..." : "Create Room"}
       </button>
